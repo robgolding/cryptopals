6f1eb5c8097176fbef18034a9feb40e034ee4855
Rob Golding <rob@robgolding.com>
2017-08-25 10:13:46 +0100

    Complete Set 1 Challenge 2 (Fixed XOR)

--- set_1/lib.py
+++ set_1/lib.py
@@ -6,3 +6,12 @@ def hex_to_base64(s):
     Converts the hex-encoded string `s` to base64.
     """
     return s.decode('hex').encode('base64').strip()
+
+
+def xor(b1, b2):
+    """
+    Returns the XOR combination of the two byte-arrays `b1` and `b2`.
+    """
+    return ''.join([
+        chr(ord(a) ^ ord(b)) for a, b in zip(b1, b2)
+    ])
--- set_1/tests.py
+++ set_1/tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from lib import hex_to_base64
+from lib import hex_to_base64, xor
 
 
 class SetOneTest(TestCase):
@@ -9,4 +9,13 @@ class SetOneTest(TestCase):
         self.assertEqual(
             hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'),
             'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t',
+        )
+
+    def test_xor(self):
+        self.assertEqual(
+            xor(
+                '1c0111001f010100061a024b53535009181c'.decode('hex'),
+                '686974207468652062756c6c277320657965'.decode('hex'),
+            ).encode('hex'),
+            '746865206b696420646f6e277420706c6179',
         )
